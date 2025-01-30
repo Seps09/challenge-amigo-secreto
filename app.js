fa6fe1e898c89a4b773fc3b99557c4b6e3ac64b6
YISUS <yndriagojesus99@gmail.com>
2025-01-29 22:42:00 -0500

    Crea logica de juego

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+nombresAmigos = []; //Array que contendra los nombre de amigos
+let resultado = document.getElementById("resultado");
+let cajaNombre = document.getElementById("amigo");
+let listado = document.getElementById("listaAmigos");
+
+
+function agregarAmigo(){
+    listado.innerHTML = "";
+    resultado.innerHTML = "";
+    nombre = cajaNombre.value;
+    if(nombre != ""){
+        nombresAmigos.push(nombre);
+        cajaNombre.value = "";
+        cajaNombre.focus();
+        actualizarListaAmigos();
+    }else{
+        alert("ingrese un nombre valido");
+    }
+    //console.log(nombresAmigos);
+}
+
+function actualizarListaAmigos(){
+
+    listado.innerHTML = nombresAmigos;
+}
+
+function sortearAmigo(){
+    let numeroSorteo = Math.floor(Math.random()*nombresAmigos.length)+0;
+    
+    resultado.innerHTML = `El amigo secreto es: ${nombresAmigos[numeroSorteo]}`
+    nombresAmigos = [];
+}
